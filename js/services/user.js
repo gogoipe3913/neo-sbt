48b6f0ecf4009af912fb1c3eded491f242bc51d8
NabeAtsu <mavinosuke@gmail.com>
2023-12-30 14:29:34 +0900

    修正

--- js/services/user.js
+++ js/services/user.js
@@ -1,8 +1,6 @@
 import { fetchGETWithRetryAsync, fetchPOSTWithRetryAsync } from '../modules/fetch.js';
 
-export async function postEntryAsync(data) {
-    const [result, errorMsg] =
-        await fetchPOSTWithRetryAsync(API_BASEURL + "entry/purchase", data);
-
-    return result
+export async function getUserAsync(data) {
+    const [result, user] = await fetchPOSTWithRetryAsync(API_BASEURL + "user", data);
+    return user
 }
